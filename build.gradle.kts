5997c525301475cbcd08e146f784cb3207dd3866
JP <jpalbert81@gmail.com>
2020-03-28 08:28:47 -0700

    add apache commons csv

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,13 +21,15 @@ repositories {
 dependencies {
     // Align versions of all Kotlin components
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+
+    implementation("org.apache.commons:commons-csv:1.8")
+
+
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-
     // Use the Kotlin JUnit integration.
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
