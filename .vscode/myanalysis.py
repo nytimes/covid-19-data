bb1d69510387f1854aa02dfe824f93e640d02525
greazer <jimgries@microsoft.com>
2020-04-07 00:29:33 -0700

    Update comments

--- .vscode/myanalysis.py
+++ .vscode/myanalysis.py
@@ -1,18 +1,17 @@
-#****************************************************************
 #  %%
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from datetime import datetime
 
-#****************************************************************
 #  %% [markdown]
+# **********************************************************************************************************
 # # Setup
 # 1. Read in the covid-19-data from nytimes for state and county (https://github.com/nytimes/covid-19-data)
 # 2. Pull in a city density table
 # 3. Pull in a state density table
 # 4. Identify list of states, counties and cities to graph
-
-#****************************************************************
+# **********************************************************************************************************
 #  %%
 state_cov_data = pd.read_csv('us-states.csv')
 county_cov_data = pd.read_csv('us-counties.csv')
@@ -46,11 +45,11 @@ county_cities_map = pd.DataFrame(county_cities, columns = ['state', 'county', 'c
 states = county_cities_map.state
 
 
-#****************************************************************
 #  %% [markdown]
+# **********************************************************************************************************
 # # State Totals
 # The total number of cases for each interesting state starting with a minimum number of cases
-#****************************************************************
+# **********************************************************************************************************
 #  %%
 def plottotalcases(state, county = 'all'):
     if county == 'all':
@@ -74,7 +73,7 @@ def plottotalcases(state, county = 'all'):
 starting_cases = 1000
 fig = plt.figure(figsize=(14,10))
 ax = fig.add_axes([0,0,1,1])
-ax.set_title('Total state cases with starting case count = ' + str(starting_cases))
+ax.set_title('Total state cases with starting case count = ' + str(starting_cases) + ' Date: ' + datetime.now().strftime('%x'))
 ax.set_xlabel('Days since hitting ' + str(starting_cases) + ' cases')
 ax.set_ylabel('Cases')
 
@@ -83,15 +82,15 @@ for s in states.unique():
 
 ax.legend()
 
-#****************************************************************
 #  %% [markdown]
-# # County Total cases by state county
+# **********************************************************************************************************
+# # County Totals
 # The total number of cases for each interesting county starting with a minimum number of cases
-
+# **********************************************************************************************************
 starting_cases = 200
 fig = plt.figure(figsize=(14,10))
 ax = fig.add_axes([0,0,1,1])
-ax.set_title('Total county cases with starting case count = ' + str(starting_cases))
+ax.set_title('Total county cases with starting case count = ' + str(starting_cases) + ' Date: ' + datetime.now().strftime('%x'))
 ax.set_xlabel('Days since hitting ' + str(starting_cases) + ' cases')
 ax.set_ylabel('Cases')
 
@@ -100,8 +99,8 @@ for p in county_cities_map.itertuples():
 
 ax.legend()
 
-#****************************************************************
 #  %% [markdown]
+# **********************************************************************************************************
 # # State per capita
 # To better get a sense of how different states may be handling the virus outbreak, you can
 # adjust the graphs to account for the number of people who live in each state. A state that has
@@ -111,13 +110,12 @@ ax.legend()
 #
 # This graph indicates that New York, New Jersey, and Louisiana, are getting far more cases per day
 # than the other states, regardless of how many people live in each state.
-
-#****************************************************************
+# **********************************************************************************************************
 #  %%
 starting_cases = 1000
 fig = plt.figure(figsize=(14,10))
 ax = fig.add_axes([0,0,1,1])
-ax.set_title('State growth per capita with starting case count = ' + str(starting_cases))
+ax.set_title('State growth per capita with starting case count = ' + str(starting_cases) + ' Date: ' + datetime.now().strftime('%x'))
 ax.set_xlabel('Days since hitting ' + str(starting_cases) + ' cases')
 ax.set_ylabel('Cases per capita')
 ax.set_ylim(0, 0.0001)
@@ -139,8 +137,8 @@ for s in states.unique():
 
 ax.legend()
 
-#****************************************************************
 #  %% [markdown]
+# **********************************************************************************************************
 # # State normalized by population density
 #
 # Each state has a population and an area in which this population lives. *Pretend* for a moment that Texas only has 100,000
@@ -152,13 +150,12 @@ ax.legend()
 
 # This graph removes this consideration from the comparison between states. As you can see, New Jersey is doing far worse than
 # than Ohio, Washington and California.
-
-#****************************************************************
+# **********************************************************************************************************
 #  %%
 starting_cases = 200
 fig = plt.figure(figsize=(14,10))
 ax = fig.add_axes([0,0,1,1])
-ax.set_title('State cases by population density starting case count = ' + str(starting_cases))
+ax.set_title('State cases by population density starting case count = ' + str(starting_cases) + ' Date: ' + datetime.now().strftime('%x'))
 ax.set_xlabel('Days since hitting ' + str(starting_cases) + ' cases')
 ax.set_ylabel('Cases by state population density')
 ax.set_ylim(0, 0.0001)
@@ -180,27 +177,30 @@ for s in states.unique():
 
 ax.legend()
 
-#****************************************************************
 #  %% [markdown]
+# **********************************************************************************************************
 # # Cities normalized by population density
 # Ohhhh, but I hear you say... Well the area of California is far larger than than of New York. Therefore, the population
-# density of California can EASILY be lower than that of New York, so this will still skew the results. In addition, each
-# state likely has a hotspot city and in those cities the population density may be FAR higher than looking at the state overall.
+# density of California can EASILY be lower than that of New York, so this skews the results to make California look better.
+# In addition, each state likely has a hotspot city and in those cities the population density may be FAR higher and thus
+# far more likely to spread a virus in the city's immediate area than the state overall.
 # In other words the population density of Los Angeles is probably FAR higher than that of California overall, so ranking
-# by state-wide density isn't fair.
+# by state-wide density isn't really a good way to compare a physically large state like California to a much smaller state
+# like NY or Ohio.
 #
 # Ok, then lets look at individual cities in each of the states!
 #
 # This graph shows that even though Detroit Michigan's population density is around 5x less than that of New York City,
-# the number of virus cases is growing there far faster than even New York and New Orleans (which both suck!). I'd be much more worried if I lived there.
-# Note that Cleveland and Seattle are pretty flat, which is really good.
-
-#****************************************************************
+# the number of virus cases is growing there far faster than even New York and New Orleans (which both suck!). I'd be much
+# more worried if I lived in Detroit right now.
+#
+# Note that Cleveland and Seattle, and Los Angeles are pretty flat, which is good.
+# **********************************************************************************************************
 #  %%
 starting_cases = 20
 fig = plt.figure(figsize=(14,10))
 ax = fig.add_axes([0,0,1,1])
-ax.set_title('City growth by population density starting case count = ' + str(starting_cases))
+ax.set_title('City growth by population density starting case count = ' + str(starting_cases) + ' Date: ' + datetime.now().strftime('%x'))
 ax.set_xlabel('Days since hitting ' + str(starting_cases) + ' cases')
 ax.set_ylabel('Cases per population density value')
 ax.set_ylim(0, 0.0001)
@@ -228,23 +228,15 @@ for p in county_cities_map.itertuples():
 
 ax.legend()
 
-# %%
-import sys
-try:
-    sys.exit(0)
-except:
-    print('Done')
-
-#******************BOKEH BOKEH BOKEH BOKEH**********************************************
 # %% [markdown]
+# **********************************************************************************************************
 # # Use Bokeh for plotting just for kicks
+# **********************************************************************************************************
 #  %%
 import bokeh.plotting as bplt
 import bokeh.models as bmod
 from bokeh.palettes import Dark2_5 as palette
 import itertools
-
-#****************************************************************
 #  %%
 starting_cases = 1000
 
@@ -277,7 +269,6 @@ for s in states.unique():
     plottotalcases_bokeh(s)
 
 bplt.show(fig_bokeh)
-#****************************************************************
 #  %%
 starting_cases = 200
 
